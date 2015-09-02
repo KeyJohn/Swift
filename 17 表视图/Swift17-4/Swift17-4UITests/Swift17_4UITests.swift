8ee08bd5edbb7dcf47734cdbe2d6d14f4f240f62
keyzhang <843387173@qq.com>
2015-09-03 03:13:24 +0800

    9.3

---
+++ 17 表视图/Swift17-4/Swift17-4UITests/Swift17_4UITests.swift
@@ -0,0 +1,35 @@
+//
+//  Swift17_4UITests.swift
+//  Swift17-4UITests
+//
+//  Created by keyzhang on 15/9/3.
+//  Copyright © 2015年 keyzhang. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class Swift17_4UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
